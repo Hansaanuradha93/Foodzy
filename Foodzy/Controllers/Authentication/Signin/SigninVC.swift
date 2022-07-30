dd7a790618cfb4fe0ebe6adf98848d03b123a5f4
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-07-30 15:20:19 +0530

    signin controller added

---
+++ Foodzy/Controllers/Authentication/Signin/SigninVC.swift
@@ -0,0 +1,16 @@
+//
+//  SigninVC.swift
+//  Foodzy
+//
+//  Created by Hansa Anuradha on 2022-07-30.
+//
+
+import UIKit
+
+class SigninVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
