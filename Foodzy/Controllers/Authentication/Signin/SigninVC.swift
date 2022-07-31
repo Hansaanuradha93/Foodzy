8c118804203bcac5eed17edcabeef630ffdd81d3
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-07-31 19:49:10 +0530

    Signin -> bug fixed

--- Foodzy/Controllers/Authentication/Signin/SigninVC.swift
+++ Foodzy/Controllers/Authentication/Signin/SigninVC.swift
@@ -16,7 +16,7 @@ class SigninVC: KeyboardHandlingVC {
     let titleLabel = FZSemiBoldLabel(text: "Hello again!", textColor: UIColor.appColor(color: .almostBlack))
     let gooleButton = FZIconButton(icon: Asserts.googleIcon, title: "Sign up with Google")
     let emailInputView = FZTextInputView(title: "Email", placeholderText: "example@gmail.com")
-    let passwordInputView = FZTextInputView(title: "Password", placeholderText: "**********")
+    let passwordInputView = FZTextInputView(isSecureTextEntry: true, title: "Password", placeholderText: "**********")
     let separatorView = FZSeparatorView()
     let signinButton = FZRegularButton(backgroundColor: UIColor.appColor(color: .mintGreen), title: "Sign In", titleColor: .white)
     let noAccountLabel = FZSemiBoldLabel(text: "Don't have an account?", textAlignment: .center, textColor: UIColor.appColor(color: .placeholderGray), fontSize: 16)
@@ -33,26 +33,28 @@ class SigninVC: KeyboardHandlingVC {
 // MARK: - Private Methods
 private extension SigninVC {
     
-    func setupLayout() {        
+    func setupLayout() {
+        view.backgroundColor = .systemBackground
+        
         let mainStackView = UIStackView(arrangedSubviews: [titleLabel, gooleButton, emailInputView, passwordInputView, separatorView, signinButton, noAccountLabel, signupButton])
         mainStackView.axis = .vertical
         mainStackView.spacing = 24
         mainStackView.setCustomSpacing(48, after: titleLabel)
         mainStackView.setCustomSpacing(24, after: gooleButton)
         
-        contentView.addSubviews(backgroundImageView, mainStackView)
-        
-        backgroundImageView.fillSuperview()
+        contentView.addSubviews(mainStackView)
         
         mainStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 140, left: GlobalConstants.padding, bottom: 0, right: GlobalConstants.padding))
     }
     
     func configureScrollView() {
         scrollView = UIScrollView()
-        scrollView.backgroundColor = .white
-        view.addSubview(scrollView)
+        scrollView.backgroundColor = .clear
+        view.addSubviews(backgroundImageView, scrollView)
         scrollView.addSubview(contentView)
         
+        backgroundImageView.fillSuperview()
+
         scrollView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
         contentView.anchor(top: scrollView.topAnchor, leading: scrollView.leadingAnchor, bottom: scrollView.bottomAnchor, trailing: scrollView.trailingAnchor)
         
