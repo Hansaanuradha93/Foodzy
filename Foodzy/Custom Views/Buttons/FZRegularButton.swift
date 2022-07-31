8dc1d7a14d3750a1607dcce5ce38f473593cabb2
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-07-31 18:58:32 +0530

    FZRegularButton designed

---
+++ Foodzy/Custom Views/Buttons/FZRegularButton.swift
@@ -0,0 +1,43 @@
+//
+//  FZRegularButton.swift
+//  Foodzy
+//
+//  Created by Hansa Anuradha on 31/07/22.
+//
+
+import UIKit
+
+class FZRegularButton: UIButton {
+
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError(ErrorMessages.fatorError)
+    }
+
+    convenience init(backgroundColor: UIColor = .white, title: String = "", titleColor: UIColor = .black, fontSize: CGFloat = 16) {
+        self.init(frame: .zero)
+        self.setup(backgroundColor: backgroundColor, title: title, titleColor: titleColor, fontSize: fontSize)
+    }
+}
+
+// MARK: - Methods
+extension FZRegularButton {
+       
+    private func setup(backgroundColor: UIColor, title: String, titleColor: UIColor, fontSize: CGFloat, borderColor: UIColor = .clear, borderWidth: CGFloat = 2) {
+        let traits = [UIFontDescriptor.TraitKey.weight: UIFont.Weight.medium]
+        var descriptor = UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.family: Fonts.montserrat])
+        descriptor = descriptor.addingAttributes([UIFontDescriptor.AttributeName.traits: traits])
+        
+        self.titleLabel?.font = UIFont(descriptor: descriptor, size: fontSize)
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.imageView?.contentMode = .scaleAspectFill
+        
+        self.setRoundedBorder(borderColor: borderColor, borderWidth: borderWidth, radius: GlobalConstants.cornerRadius)
+    }
+}
