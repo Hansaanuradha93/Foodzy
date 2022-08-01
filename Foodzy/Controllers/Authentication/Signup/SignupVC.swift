9a20bd29f22e6bafd99ad3f67789f4630bfb3434
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-08-01 22:27:11 +0530

    Signup -> partially designed

---
+++ Foodzy/Controllers/Authentication/Signup/SignupVC.swift
@@ -0,0 +1,72 @@
+//
+//  SignupVC.swift
+//  Foodzy
+//
+//  Created by Hansa Anuradha on 01/08/22.
+//
+
+import UIKit
+
+class SignupVC: KeyboardHandlingVC {
+    
+    // MARK: Properties
+    private let contentView = UIView()
+    
+    let backgroundImageView = UIImageView(image: Asserts.signupBackground)
+    let titleLabel = FZSemiBoldLabel(text: "Sign Up", textColor: UIColor.appColor(color: .almostBlack))
+    let subtitleLabel = FZSemiBoldLabel(text: "Please enter your details", textColor: UIColor.appColor(color: .placeholderGray), fontSize: 16)
+    let gooleButton = FZIconButton(icon: Asserts.googleIcon, title: "Sign up with Google")
+    let otherOptionsButton = FZRegularButton(backgroundColor: .white, title: "Other Options Sign Up", titleColor: UIColor.appColor(color: .mintGreen), borderColor: UIColor.appColor(color: .mintGreen))
+
+    let nameInputView = FZTextInputView(title: "Name", placeholderText: "Thomas")
+    let emailInputView = FZTextInputView(title: "Email", placeholderText: "example@gmail.com")
+    let passwordInputView = FZTextInputView(isSecureTextEntry: true, title: "Password", placeholderText: "At least 8 characters")
+    let separatorView = FZSeparatorView()
+    
+    
+    let signupButton = FZRegularButton(backgroundColor: UIColor.appColor(color: .mintGreen), title: "Sign Up", titleColor: .white)
+    let alreadyHaveAccountLabel = FZSemiBoldLabel(text: "Already have an account?", textAlignment: .center, textColor: UIColor.appColor(color: .placeholderGray), fontSize: 16)
+    let signinButton = FZRegularButton(backgroundColor: .white, title: "Sign In", titleColor: UIColor.appColor(color: .mintGreen), borderColor: UIColor.appColor(color: .mintGreen))
+
+    // MARK: View Controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureScrollView()
+        setupLayout()
+    }
+}
+
+// MARK: - Private Methods
+private extension SignupVC {
+    
+    func setupLayout() {
+        view.backgroundColor = .systemBackground
+        
+        let mainStackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel, gooleButton, otherOptionsButton, nameInputView, emailInputView, passwordInputView, separatorView, signupButton, alreadyHaveAccountLabel, signinButton])
+        mainStackView.axis = .vertical
+        mainStackView.spacing = 24
+        mainStackView.setCustomSpacing(12, after: titleLabel)
+        mainStackView.setCustomSpacing(48, after: subtitleLabel)
+
+        contentView.addSubviews(mainStackView)
+
+        mainStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 24, left: GlobalConstants.padding, bottom: 0, right: GlobalConstants.padding))
+    }
+    
+    func configureScrollView() {
+        scrollView = UIScrollView()
+        scrollView.backgroundColor = .clear
+        view.addSubviews(backgroundImageView, scrollView)
+        scrollView.addSubview(contentView)
+        
+        backgroundImageView.fillSuperview()
+
+        scrollView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        contentView.anchor(top: scrollView.topAnchor, leading: scrollView.leadingAnchor, bottom: scrollView.bottomAnchor, trailing: scrollView.trailingAnchor)
+        
+        NSLayoutConstraint.activate([
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 850)
+        ])
+    }
+}
