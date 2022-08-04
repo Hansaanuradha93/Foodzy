66a85f0dbf78904be5b9f14d7030ace7bf236887
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-08-04 21:54:39 +0530

    Tab views added

---
+++ Foodzy/Controllers/Profile/ProfileVC.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileVC.swift
+//  Foodzy
+//
+//  Created by Hansa Anuradha on 04/08/22.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
